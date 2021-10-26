e0467841ee38f1f68c4eb54be9b8f28751f5b3c1
IMRJ-1998 <59196366+IMRJ-1998@users.noreply.github.com>
2021-10-27 04:05:29 +0530

    Add files via upload

---
+++ Blind RSA Voting/BlindSig.py
@@ -0,0 +1,68 @@
+import cryptomath, random, hashlib
+
+
+class Signer:
+
+    def __init__(self):
+        self.publicKey, self.privateKey = (self.generateInformation())
+
+    def generateInformation(self):
+        # Generates public and private keys and saves them to a file.
+        p = cryptomath.findPrime()
+        q = cryptomath.findPrime()
+        phi = (p - 1) * (q - 1)
+        n = p * q
+
+        foundEncryptionKey = False
+        while not foundEncryptionKey:
+            e = random.randint(2, phi - 1)
+            if cryptomath.gcd(e, phi) == 1:
+                foundEncryptionKey = True
+
+        d = cryptomath.findModInverse(e, phi)
+
+        publicInfo = {"n": n, "e": e}
+        privateInfo = {"n": n, "d": d}
+
+        return [(publicInfo), (privateInfo)]
+
+    def getPublicKey(self):
+        return self.publicKey
+
+    def signMessage(self, message, eligible):
+        if eligible == "y":
+            return pow(message, self.privateKey['d'], self.publicKey['n'])
+        else:
+            return None
+
+    def verifyVoter(self, eligible):
+        pass
+
+
+class Voter:
+    def __init__(self, n, eligible):
+        self.eligible = eligible
+
+        foundR = False
+        while not foundR:
+            self.r = random.randint(2, n - 1)
+            if cryptomath.gcd(self.r, n) == 1:
+                foundR = True
+
+    def blindMessage(self, m, n, e):
+
+        blindMessage = (m * pow(self.r, e, n)) % n
+
+        return blindMessage
+
+    def unwrapSignature(self, signedBlindMessage, n):
+        rInv = cryptomath.findModInverse(self.r, n)
+
+        return ((signedBlindMessage * rInv) % n)
+
+    def getEligibility(self):
+        return self.eligible
+
+
+def verifySignature(message, randNum, signature, publicE, publicN):
+    return (int(hashlib.sha256((str(message) + str(randNum)).encode('utf-8')).hexdigest(), 16) == pow(signature, publicE, publicN))
---
+++ Blind RSA Voting/ws_app.py
@@ -0,0 +1,120 @@
+import os
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+clients = []
+
+
+class SimpleChat(WebSocket):
+    yes_num = 0
+    no_num = 0
+
+    def handleMessage(self):
+        if "Blinded message" in self.data:
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        if "Decoded message" in self.data:
+            answer = int(self.data[-1])
+            if (answer == 1):
+                SimpleChat.yes_num += 1
+            else:
+                SimpleChat.no_num += 1
+
+        print(self.data)
+        print("")
+
+        if "Verification" in self.data:
+            print("")
+            print("")
+            print("")
+            print("")
+            print(
+                "------------------------------------------------------------------------`               YES VOTES: " + str(
+                    SimpleChat.yes_num) + " NO VOTES: " + str(
+                    SimpleChat.no_num) + "               ---------------------------------------------------------------------------")
+            print("")
+            print("")
+            print("")
+            print("")
+
+        for client in clients:
+            if client != self:
+                client.sendMessage(self.address[0] + u' - ' + self.data)
+
+    def handleConnected(self):
+        # print(self.address, 'connected')
+        for client in clients:
+            client.sendMessage(self.address[0] + u' - connected')
+        clients.append(self)
+
+    def handleClose(self):
+        clients.remove(self)
+        # print(self.address, 'closed')
+        for client in clients:
+            client.sendMessage(self.address[0] + u' - disconnected')
+
+
+server = SimpleWebSocketServer('', 8000, SimpleChat)
+os.system('cls' if os.name == 'nt' else 'clear')
+welcomeString = "Public Polling Center Listener"
+print('{:-^204}'.format(""))
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                 Public Polling Center Listener                                                                                                   -----")
+print(
+    "-----                                                                     RSA Blind Signature                                                                                                          -----")
+print(
+    "-----                                                                       Voting Software                                                                                                            -----")
+print(
+    "-----                                                                            V1.0                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+print(
+    "-----                                                                                                                                                                                                  -----")
+
+print('{:-^204}'.format(""))
+server.serveforever()
